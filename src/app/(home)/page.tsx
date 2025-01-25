9b29bafe5f0c82565a9bf53a6ed626e7a5136578
Abhay2004Kumar <abs220502@gmail.com>
2025-01-25 15:48:06 +0530

    fixing

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,6 +8,8 @@ import { getAllCategories, getProductsByTag, getProductsForCard } from "@/lib/ac
 import data from "@/lib/data"
 import { toSlug } from "@/lib/utils"
 export default async function Page(){
+  
+
     const categories = (await getAllCategories()).slice(0, 4)
     const newArrivals = await getProductsForCard({
       tag: 'new-arrival',
